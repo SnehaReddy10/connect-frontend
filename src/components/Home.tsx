cc7259d277d5cb4099c78d3da9b1346dcd016739
Sneha Reddy <snehareddy9178@gmail.com>
2024-06-18 22:47:43 +0530

    fix hero component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -10,11 +10,13 @@ const Home = () => {
         <p className="p-4">Report, Brainstorm, Innovate, Resolve</p>
       </div>
 
-      <div className="mt-5 border h-80 border-slate-300 w-[48%] px-4 py-3 rounded-sm shadow-lg shadow-slate-900 hover:bg-slate-700 transition ease">
-        <h4>{issue.title}</h4>
-        <p>{issue.description}</p>
-        <p>{issue.comments}</p>
-      </div>
+      {issue && (
+        <div className="mt-5 border h-80 border-slate-300 w-[48%] px-4 py-3 rounded-sm shadow-lg shadow-slate-900 hover:bg-slate-700 transition ease">
+          <h4>{issue.title}</h4>
+          <p>{issue.description}</p>
+          <p>{issue.comments}</p>
+        </div>
+      )}
 
       <div className="mt-10 pb-10 mb-10">
         <Issues />
